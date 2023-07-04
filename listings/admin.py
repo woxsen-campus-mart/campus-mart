93743b4d9f6644c35b3ee81d70f2331d375403d9
IbneRaveem <86484044+IbneRaveem@users.noreply.github.com>
2023-07-04 19:47:37 +0530

    Update admin.py
    Signed-off-by: IbneRaveem <86484044+IbneRaveem@users.noreply.github.com>

--- listings/admin.py
+++ listings/admin.py
@@ -5,7 +5,7 @@ class ListAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'owner', 'category', 'price', 'is_published', 'list_date')
     list_display_links = ('id', 'title')
     list_filter = ('category',)
-    search_fields = ('title')
+    search_fields = ('title', 'description', 'address', 'city', 'zipcode', 'state', 'price')
     list_per_page = 30
 
 admin.site.register(Listing, ListAdmin)
